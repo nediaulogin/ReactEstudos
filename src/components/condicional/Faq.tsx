9f42c5299684b3ff88db2065d00aef8972e4837c
Elvis <nediaulog.gmail.com>
2023-08-30 07:25:07 -0300

    outra forma de alterar o estado

--- src/components/condicional/Faq.tsx
+++ src/components/condicional/Faq.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import Pergunta from "./Pergunta";
 
 export default function Faq() {
+    const [ativo, setAtivo] = useState<number>(0)
+    function alternarVisibilidade(indice: number) {
+        if (indice === ativo) {
+            setAtivo(-1)
+        } else {
+            setAtivo(indice)
+        }
+    }
     return (
         <div className="flex flex-col gap-2 w-[90%]  md:w-3/4">
-            <Pergunta texto="Quem descobriu o Brasil" resposta="Lorem ipsum sit..." />
-            <Pergunta texto="O que é Typescript" resposta="Lorem ipsum sit..." />
-            <Pergunta texto="React é um frame work" resposta="Lorem ipsum sit..." />
+            <Pergunta
+                indice={0}
+                aberta={ativo === 0}
+                texto="Quem descobriu o Brasil" resposta="Lorem ipsum sit..."
+                alternarVisibilidade={alternarVisibilidade} />
+            <Pergunta
+                indice={1}
+                aberta={ativo === 1}
+                texto="O que é Typescript" resposta="Lorem ipsum sit..."
+                alternarVisibilidade={alternarVisibilidade}
+            />
+            <Pergunta
+                indice={2}
+                aberta={ativo === 2}
+                texto="React é um frame work" resposta="Lorem ipsum sit..."
+                alternarVisibilidade={alternarVisibilidade}
+            />
         </div>
     )
 }
--- src/components/condicional/Pergunta.tsx
+++ src/components/condicional/Pergunta.tsx
@@ -1,27 +1,29 @@
 import { IconChevronDown, IconChevronUp } from "@tabler/icons-react"
-import { useState } from "react"
 import If from "./if"
 
 interface PerguntaProps {
+    indice: number
     texto: string
     resposta: string
+    aberta: boolean
+    alternarVisibilidade: (indice: number) => void
 }
 export default function Pergunta(props: PerguntaProps) {
 
-    const [aberta, setAberta] = useState<boolean>(false)
+    // const [aberta, setAberta] = useState<boolean>(false)
 
     return (
         <div className="overflow-hidden border border-white rounded-md">
             <div className="flex justify-between p-5 cursor-pointer bg-zinc-700"
-                onClick={() => setAberta(!aberta)}>
+                onClick={() => props.alternarVisibilidade(props.indice)}>
                 <span>{props.texto}</span>
-                {/* <IfElse teste={aberta}>
+                {/* <IfElse teste={props.aberta}>
                     <IconChevronUp />
                     <IconChevronDown />
                 </IfElse> */}
-                {aberta ? <IconChevronUp /> : <IconChevronDown />}
+                {props.aberta ? <IconChevronUp /> : <IconChevronDown />}
             </div>
-            <If teste={aberta}>
+            <If teste={props.aberta}>
                 <div className="p-5">
                     {props.resposta}
                 </div>
